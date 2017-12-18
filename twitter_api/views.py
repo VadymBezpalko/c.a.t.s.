cc59af75686426b7682b2260b2cf16f577b63727
v <robozorro@mail.ru>
2017-12-18 05:21:17 +0100

    fetch many tweets

--- twitter_api/views.py
+++ twitter_api/views.py
@@ -19,58 +19,90 @@ API_SECRET = '3CB6gOeqqpYLQ1Y1rlkVbvriCQjdyg3pXfq3kSpdjIR9kToSxj'
 TOKEN_KEY = '3365870121-PcBq6nthIQRWrcGwNRg1xUW5LGaOJ9CLd8262Ie'
 TOCKET_SECRET_KEY = 'AVUuT3QGFPK6x5icab7aSdI9fjC0Cje2tkLVsnIhfX9yI'
 
-api = Twython(API_KEY, API_SECRET, TOKEN_KEY, TOCKET_SECRET_KEY)
+api = Twython(API_KEY, API_SECRET)
 
 
 @api_view(['POST'])
 def fetch_statuses(request):
-    tweets = api.search(q=request.data['search'],
-                        tweet_mode='extended',
-                        since=request.data['since'],
-                        until=request.data['until'],
-                        count=request.data['count'])
+    count_of_tweets_to_be_fetched = int(request.data['count'])
+    tweets_length = 0
+    for i in range(0, int(count_of_tweets_to_be_fetched / 100) + 1):
+        print(i, '******************************')
+        if int(count_of_tweets_to_be_fetched) < tweets_length:
+            break
 
-    for tweet in tweets['statuses']:
-        print('-------------------')
-        print(tweet['full_text'])
-        if 'retweeted_status' in tweet:
-            print('retweeted')
-            tweet_text = tweet['retweeted_status']['full_text']
-            tweet_id = tweet['retweeted_status']['id_str']
-            created_at = tweet['retweeted_status']['created_at']
-            retweeted_at = tweet['created_at']
+        if i == 0:
+            results = api.search(q=request.data['search'],
+                                 tweet_mode='extended',
+                                 since=request.data['since'],
+                                 until=request.data['until'],
+                                 count=100)
         else:
-            print('original tweet')
-            tweet_text = tweet['full_text']
-            tweet_id = tweet['id_str']
-            created_at = tweet['created_at']
-            retweeted_at = None
+            # After the first call we should have max_id from result of previous call. Pass it in query.
+            results = api.search(q=request.data['search'],
+                                 tweet_mode='extended',
+                                 count=100,
+                                 since=request.data['since'],
+                                 include_entities='true',
+                                 max_id=next_max_id)
 
+            # STEP 2: Save the returned tweets
+        for tweet in results['statuses']:
+            if int(count_of_tweets_to_be_fetched) < tweets_length:
+                break
+            print('-------------------')
+            print(tweets_length)
+            print(tweet['full_text'])
+            print(tweet['created_at'])
+            new_tweet = True
+            if 'retweeted_status' in tweet:
+                print('retweeted')
+                tweet_text = tweet['retweeted_status']['full_text']
+                tweet_id = tweet['retweeted_status']['id_str']
+                created_at = tweet['retweeted_status']['created_at']
+                retweeted_at = tweet['created_at']
+            else:
+                print('original tweet')
+                tweet_text = tweet['full_text']
+                tweet_id = tweet['id_str']
+                created_at = tweet['created_at']
+                retweeted_at = None
+
+            try:
+                temp = TwitterData.objects.get(status_id=tweet_id)
+                if tweet['retweet_count'] > temp['retweet_count']:
+                    serializer = TwitterDataSerializer(temp, data={"retweet_count": tweet["retweet_count"]})
+                    new_tweet = False
+                print('juz jest taki tweet')
+
+            except TwitterData.DoesNotExist:
+                print('tworzę nowy tweet')
+                serializer = TwitterDataSerializer(data={
+                    'status_id': tweet_id,
+                    'text': tweet_text,
+                    # 'translated_text': translate_text(tweet_text),
+                    'retweet_count': tweet['retweet_count'],
+                    'created_at': format_date(created_at),
+                    'retweeted_at': format_date(retweeted_at) if retweeted_at is not None else None,
+                    'user': {
+                        'user_id': tweet['user']['id_str'],
+                        'name': tweet['user']['name']
+                    }
+                })
+            if new_tweet and serializer.is_valid():
+                serializer.save()
+                tweets_length += 1
+            else:
+                return JsonResponse(serializer.errors, status=400, safe=False)
+
+            # STEP 3: Get the next max_id
         try:
-            temp = TwitterData.objects.get(status_id=tweet_id)
-            serializer = TwitterDataSerializer(temp, data={"retweet_count": tweet["retweet_count"]})
-            print(temp)
-            print('juz jest taki tweet')
-
-        except TwitterData.DoesNotExist:
-            print('tworzę nowy tweet')
-            serializer = TwitterDataSerializer(data={
-                'status_id': tweet_id,
-                'text': tweet_text,
-                # 'translated_text': translate_text(tweet_text),
-                'retweet_count': tweet['retweet_count'],
-                'created_at': format_date(created_at),
-                'retweeted_at': format_date(retweeted_at) if retweeted_at is not None else None,
-                'user': {
-                    'user_id': tweet['user']['id_str'],
-                    'name': tweet['user']['name']
-                }
-            })
-        print(type(serializer))
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            return JsonResponse(serializer.errors, status=400, safe=False)
+            # Parse the data returned to get max_id to be passed in consequent call.
+            next_results_url_params = results['search_metadata']['next_results']
+            next_max_id = next_results_url_params.split('max_id=')[1].split('&')[0]
+        except:
+            # No more next pages
+            break
 
     serializer = TwitterDataSerializer(TwitterData.objects.all(), many=True)
 
