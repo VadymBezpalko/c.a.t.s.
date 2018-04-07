7f0c574c7ff2c656bc2c636e19b4330e3599ee85
VadymBezpalko <ozzy2204@gmail.com>
2018-04-07 14:47:58 +0200

    TextBlob NER

--- named_entity/urls.py
+++ named_entity/urls.py
@@ -4,4 +4,5 @@ from named_entity import views
 urlpatterns = [
     url(r'^named_entity/$', views.get_ner_data),
     url(r'^named_entity/process$', views.ner_process),
+    url(r'^named_entity/tb_process', views.ner_textblob_process),
 ]
--- named_entity/views.py
+++ named_entity/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from nltk import ne_chunk, pos_tag, word_tokenize
 from nltk.tree import Tree
 from rest_framework.decorators import api_view
-
+from textblob import TextBlob
 from twitter_api.models import TwitterData
 from named_entity.serializers import NamedEntityDataSerializer
 from named_entity.models import NamedEntityData
@@ -57,6 +57,41 @@ def get_continuous_chunks(text):
             else:
                     continue
     return continuous_chunk
+
+
+@api_view(['GET'])
+def ner_textblob_process(request):
+    search_term = request.GET.get('search_term', None)
+    twitter_data = TwitterData.objects(search_term=search_term)
+
+    for tweet in twitter_data:
+        sentiment = tweet['sentimental']['polarity']
+        entities = TextBlob(tweet['translated_text'])
+        for entity in entities.noun_phrases:
+            temp_entity = entity.lower()
+            try:
+                temp = NamedEntityData.objects.get(text=temp_entity)
+
+                if sentiment >= 0:
+                    serializer = NamedEntityDataSerializer(temp, data={"pos_quantity": temp['pos_quantity'] + 1})
+                else:
+                    serializer = NamedEntityDataSerializer(temp, data={"neg_quantity": temp['neg_quantity'] + 1})
+
+            except NamedEntityData.DoesNotExist:
+                serializer = NamedEntityDataSerializer(data={
+                    'text': temp_entity,
+                    'search_term': search_term,
+                    'pos_quantity': 1 if sentiment >= 0 else 0,
+                    'neg_quantity': 1 if sentiment < 0 else 0
+                })
+
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                return JsonResponse(serializer.errors, status=400, safe=False)
+
+    return JsonResponse(
+        NamedEntityDataSerializer(NamedEntityData.objects(search_term=request.GET.get('search_term', None)), many=True).data, safe=False)
 
 
 @api_view(['GET'])
